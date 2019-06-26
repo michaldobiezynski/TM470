78f432767536910ff94052e4ab8866c17bb8ce02
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-06-26 17:22:06 +0100

    initial commit

---
+++ src/main/java/com/tm470/WoodMacPark/WoodMacParkApplication.java
@@ -0,0 +1,13 @@
+package com.tm470.WoodMacPark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodMacParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodMacParkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tm470/WoodMacPark/WoodMacParkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tm470.WoodMacPark;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WoodMacParkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
