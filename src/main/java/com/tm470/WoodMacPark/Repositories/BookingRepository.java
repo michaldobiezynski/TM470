adb081e85b841998ad68490f64eeeac2859a49ba
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-06-30 14:52:03 +0100

    created view for displaying list of all accounts

--- src/main/java/com/tm470/WoodMacPark/Repositories/BookingRepository.java
+++ src/main/java/com/tm470/WoodMacPark/Repositories/BookingRepository.java
@@ -4,7 +4,7 @@ import com.tm470.WoodMacPark.Models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface BookingRepository extends JpaRepository<Booking, Integer> {
+
+public interface BookingRepository  {
 
 }
